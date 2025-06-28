705d395cdfb190e9c2f9294aa3a5e9c951082a9b
MichaelFisher1997 <contact@micahelfisher.tech>
2025-06-28 21:17:42 +0100

    Refactored backend

--- build.sh
+++ build.sh
@@ -6,6 +6,16 @@ echo "Generating templ files..."
 cd app && templ generate -path ./views && cd ..
 
 echo "Building Tailwind CSS..."
-tailwindcss -i app/static/css/styles.css -o app/static/css/output.css --minify
+tailwindcss -i /home/micqdf/Documents/findser/transogov2/app/static/css/styles.css -o /home/micqdf/Documents/findser/transogov2/app/static/css/output.css --minify
+
+echo "Building templ..."
+templ generate /home/micqdf/Documents/findser/transogov2/app
+
+echo "Build Go"
+go build -o transogov2 /home/micqdf/Documents/findser/transogov2/app
 
 echo "Local build complete!"
+
+echo "Running Go tests"
+
+cd app && go test -v
